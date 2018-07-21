b577361b5fc85f05623c30d3f82767c886119eb0
kopringo <kopringo@gmail.com>
2018-07-21 23:40:33 +0200

    reading files using chrome API

--- ChromeApp/background.js
+++ ChromeApp/background.js
@@ -7,3 +7,14 @@ chrome.app.runtime.onLaunched.addListener(function() {
   });
 
 });
+
+chrome.extension.isAllowedIncognitoAccess(function(isAllowedAccess) {
+  if (isAllowedAccess) return; // Great, we've got access
+
+  // alert for a quick demonstration, please create your own user-friendly UI
+  alert('Please allow incognito mode in the following screen.');
+
+  chrome.tabs.create({
+      url: 'chrome://extensions/?id=' + chrome.runtime.id
+  });
+});
--- ChromeApp/client.js
+++ ChromeApp/client.js
@@ -3,6 +3,8 @@ $(function(){
 	var $files = $('#filesPanel');
 	var $preview = $('#previewPanel');
 
+	var f = {};
+
 	function exportToFileEntry(fileEntry) {
 							console.log(fileEntry);
 							console.log('-------');
@@ -11,9 +13,31 @@ $(function(){
 	directoryReader.readEntries(function(entries) {
 		console.log(entries);
 		for(var i in entries){
-			$files.append($('<span class="photo" data-path="' + entries[i].fullPath + '">' + entries[i].name + '</span><br/>'));
+			
+			f[entries[i].name] = entries[i];
+			chrome.fileSystem.getDisplayPath(entries[i], function(displayPath){
+				//$files.append($('<span class="photo" data-path="' + displayPath + '">' + entries[i].name + '</span><br/>'));
+			});
+			$files.append($('<span class="photo" data-path="' + entries[i].name + '">' + entries[i].name + '</span><br/>'));
 		}
 	});
+
+	console.log('!');
+	fileEntry.getFile('400.jpg', {}, function(f){
+		f.file(function(file){
+			var reader = new FileReader();
+			reader.onloadend = function(e) {
+				console.log(e);
+			};
+			reader.readAsText(file);
+		});
+		
+		//console.log(f);
+		//f.createReader(function(fileReader){
+		//	console.log('mam reader!');
+		//});
+	});
+
 	fileEntry.getFile('dupadupa.txt', {create: true}, function(f){
 		f.createWriter(function(fileWriter) {
 
@@ -40,8 +64,36 @@ $(function(){
         }, exportToFileEntry);
 		
 	$(document).on('click', '.photo', function(){
-		var fullPath = $(this).data('path');
-		$preview.html($('<img src="' + fullPath + '" style="width: 100%;"/>'))
+
+		console.log(f);
+		console.log(f[$(this).data('path')].file);
+
+		
+		var reader = new FileReader();
+		reader.onloadend = function (e) {
+				console.log('loaded');
+				successCallback(e.target.result);
+		};
+		reader.readAsText( f[$(this).data('path')].file );
+		
+
+		/*
+		var fullPath = 'file://' + $(this).data('path');
+		//$preview.html($('<img src="file://' + fullPath + '" style="width: 100%;"/>'))
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', fullPath, true);
+		xhr.responseType = 'blob';
+		xhr.onload = function(e) {
+  		var img = document.createElement('img');
+			img.src = window.URL.createObjectURL(this.response);
+			$preview.html($(img));
+  		//document.body.appendChild(img);
+		};
+
+		xhr.send();
+*/
+
 
 	});
 
